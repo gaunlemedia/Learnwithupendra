c2f7e8976340281492284aaf094d3df1f68ade61
gaunlemedia <upendrabinadi123@gmail.com>
2026-07-14 00:08:25 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function startQuiz() {
-  alert("Welcome to Learn with Upendra!\n\nQuiz section will be available soon.");
-}
+// Learn with Upendra
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    alert("📚 Welcome to Learn with Upendra!");
+
+});
+
+const btn = document.querySelector(".hero button");
+
+btn.addEventListener("click", function () {
+
+    alert("🚀 English Learning Section Coming Soon!");
+
+});
